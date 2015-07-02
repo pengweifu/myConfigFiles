63d96dd11dafedb2d557fdf748197d4504b75a70
pengweifu <987528260@qq.com>
2015-07-02 23:42:23 +0800

    editplus config

---
+++ editplus/template.php
@@ -0,0 +1,20 @@
+<?PHP
+//^!
+Require("inc/.php");
+
+$Mysql = New DBClass();
+
+$action=StrToLower(Toget("action"));
+Switch ($action){
+Case "":
+	();
+	Break;
+Default:
+	();
+	Break;
+}
+/*---------------------------------*/
+Function _Del(){
+	Global $Mysql;
+}
+?>
