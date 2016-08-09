f3fcfaead1cd5dced35f6faeea8b88da4176819a
pengweifu <987528260@qq.com>
2016-08-10 01:42:44 +0800

    Update gitAutoUpdate.php

--- linuxshell/gitAutoUpdate.php
+++ linuxshell/gitAutoUpdate.php
@@ -1,9 +1,15 @@
 <?php
+// this file is located in /home/william/workspace/PHP/wapacom/hook/
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 // sudo vim /etc/sudoers 
-// add `%www-data ALL=(ALL) ALL` to sudoers and save
-$target = '/home/william/workspace/PHP/wapacom';
-exec("cd $target");
-$cmd = "echo '992210' | sudo -Hu www-data git pull 2>$target/log.txt";
-shell_exec($cmd);
+// add `%www-data ALL=(ALL:ALL) NOPASSWD:/home/william/workspace/PHP/wapacom/hook/pull.sh` to sudoers and save
+shell_exec('sudo ./pull.sh');
+
+/*
+* contents of pull.sh
+*
+* #!/bin/sh
+* cd /home/william/workspace/PHP/wapacom
+* /usr/bin/git pull >>/home/william/workspace/PHP/wapacom/hook/pull.log 2>&1
+*/
