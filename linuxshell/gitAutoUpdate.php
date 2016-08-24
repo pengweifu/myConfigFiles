a56e8de8a9efbb0f3f8b647956749e20a447caf7
pengweifu <987528260@qq.com>
2016-08-24 13:58:33 +0800

    Update gitAutoUpdate.php

--- linuxshell/gitAutoUpdate.php
+++ linuxshell/gitAutoUpdate.php
@@ -6,7 +6,7 @@ if (empty($data)) {
 	echo phpinfo();
 } else {
 	if ($data['branch'] == 'master') {
-		shell_exec('sudo ./pull.sh');
+		shell_exec('sudo ./pull.sh');//shell_exec('sudo /home/william/Git/weike/hook/pull.sh');
 	} else {
 		file_put_contents('pull.log', time() . "\n", FILE_APPEND);
 	}
@@ -17,6 +17,7 @@ if (empty($data)) {
 * contents of pull.sh
 *
 * #!/bin/sh
+* # sudo -Hu www-data pwd && cd /home/william/Git/weike && /usr/bin/git pull >>/home/william/Git/weike/hook/pull.log 2>&1
 * cd /home/william/workspace/PHP/wapacom
 * /usr/bin/sudo -Hu www-data /usr/bin/git pull >>/home/william/workspace/PHP/wapacom/hook/pull.log 2>&1
 */
